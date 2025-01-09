9a8d636271979ca0f44ae9182a76aebf0d3c6340
Phuong Nguyen <tronganhnguyen222@gmail.com>
2025-01-09 17:28:47 +0700

    [UPDATE][FE] build download files

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+document.querySelector(".form-control").addEventListener("change", function(e){
+  let files = e?.target?.files[0]
+  let uploads = URL.createObjectURL(files)
+  document.querySelector("#upload-file").innerHTML += `
+   <details>
+      <summary>${files?.name}</summary>
+      <a href="${uploads}">
+        <p class="ml-5">Download files</p>
+      </a>
+   </details>
+  `
+})
